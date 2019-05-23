90123705df05241dd3caed87c7fb5f02c737e81f
mustafa oğuzhan ün <mustafaoguzhanun@mustafa-MacBook-Air.local>
2019-05-23 18:11:06 +0300

    initial commit

---
+++ src/main/java/com/oguzhanun/springboot/herokuapp/HerokuAppApplication.java
@@ -0,0 +1,24 @@
+package com.oguzhanun.springboot.herokuapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
+
+
+@Controller
+@SpringBootApplication
+public class HerokuAppApplication {
+
+	@RequestMapping("/")
+    @ResponseBody
+	String home() 
+	{
+      return "Hello World!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuAppApplication.class, args);
+	}
+
+}
